e2b32e24f8978ee84fd6cbc57cbb5a4d3efb22fc
Burte Bayarsaikhan <burte.bsn@gmail.com>
2024-02-05 04:29:14 +0800

    Setup project

---
+++ NeoCafe Client/Register/RegisterView.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterView.swift
+//  NeoCafe Client
+//
+//  Created by Burte Bayaraa on 2024.02.02.
+//
+
+import UIKit
+import Foundation
+import SnapKit
+
+class RegisterView: UIView {
+
+}
--- NeoCafe Client/ViewController.swift
+++ NeoCafe Client/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SkeletonView
 
 class ViewController: UIViewController {
-
+    
+    lazy var myView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isSkeletonable = true
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        self.view.addSubview(myView)
+        myView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        myView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        myView.showAnimatedSkeleton()
     }
-
-
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        myView.showSkeleton()
+//    }
 }
+
+
+public protocol SkeletonTableViewDataSource: UITableViewDataSource {
+    func numSections(in collectionSkeletonView: UITableView) -> Int // Default: 1
+    func collectionSkeletonView(_ skeletonView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier
+    func collectionSkeletonView(_ skeletonView: UITableView, skeletonCellForRowAt indexPath: IndexPath) -> UITableViewCell? // Default: nil
+    func collectionSkeletonView(_ skeletonView: UITableView, prepareCellForSkeleton cell: UITableViewCell, at indexPath: IndexPath)
+}
+
+func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
+   return "CellIdentifier"
+}
+
 
